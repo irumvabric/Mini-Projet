6215a8fc83c5f558d3d7fb3e82f4c6d8f57c1f34
irumvabric <irumvabric#@gmail.com>
2025-01-29 13:13:37 +0200

    Edit Models

--- rendez_vous/models.py
+++ rendez_vous/models.py
@@ -4,30 +4,20 @@ from django.contrib.auth.models import User
 
 class Patient(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    location_name = models.CharField(max_length=255)
-    temperature = models.FloatField()
-    humidity = models.IntegerField()
-    wind_speed = models.FloatField(null=True, blank=True)
-    condition = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-class Doctor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    location_name = models.CharField(max_length=255)
-    temperature = models.FloatField()
-    humidity = models.IntegerField()
-    wind_speed = models.FloatField(null=True, blank=True)
-    condition = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    horaire = models.ForeignKey(Horaire,on_delete=models.PROTECT)
+    gender = models.enums('M','F')
+    adress = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
     
 class Horaire(models.Model):
-    id_doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
     location_name = models.CharField(max_length=255)
     temperature = models.FloatField()
     humidity = models.IntegerField()
@@ -40,13 +30,37 @@ class Horaire(models.Model):
 
     
 
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    speciality = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    horaire = models.ForeignKey(Horaire,on_delete=models.PROTECT)
+    gender = models.enums('M','F')
+    adress = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
 class Assistant_Doctor(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    location_name = models.CharField(max_length=255)
-    temperature = models.FloatField()
-    humidity = models.IntegerField()
-    wind_speed = models.FloatField(null=True, blank=True)
-    condition = models.CharField(max_length=255)
+    id_doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.enums('M','F')
+    adress = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -56,12 +70,11 @@ class Assistant_Doctor(models.Model):
 class Rendez_vous(models.Model):
     id_doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
     id_patient = models.ForeignKey(Patient,on_delete=models.PROTECT)
-    location_name = models.CharField(max_length=255)
-    temperature = models.FloatField()
-    humidity = models.IntegerField()
-    wind_speed = models.FloatField(null=True, blank=True)
-    condition = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    date = models.DateField()
+    description = models.TextField()
+    heure_arrive = models.TimeField()
+    heure_fin = models.TimeField()
+    status = models.enums('pending','confirmed','canceled')
 
     def __str__(self):
         return self.name
