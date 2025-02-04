9c0e8b55b043b2e3b224de095d66a21e63ecd30e
irumvabric <irumvabric#@gmail.com>
2025-02-04 12:59:22 +0200

    Made more that one migrations

--- rendez_vous/models.py
+++ rendez_vous/models.py
@@ -21,6 +21,7 @@ class Rendez_vousConst(models.TextChoices):
      
 
 class User(User):
+    # id_user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     # email = models.EmailField(max_length=70,unique=True)
@@ -29,64 +30,72 @@ class User(User):
     
 
 class Patient(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=10,choices=GenderConst,default=GenderConst.Male)
     adress = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
-    
+    created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return f"{self.user.first_name} {self.user.last_name}"
     
 
 class Doctor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     speciality = models.CharField(max_length=100)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=10,choices=GenderConst,default=GenderConst.Male)
     adress = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return self.name
+        return f"{self.user.first_name} {self.user.last_name}"
     
 
 class Availability(models.Model):
-    id_doctor = models.ForeignKey(Doctor,max_length=255,on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor,max_length=255,on_delete=models.CASCADE)
     day = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     status = models.CharField(max_length=10,choices=AvailabilityConst,default=AvailabilityConst.Busy)
+    created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return self.name
+        return f"Assistant - {self.doctor}"
 
     
 
 class Assistant_Doctor(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    id_doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
-    surname = models.CharField(max_length=100)
+    doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=10,choices=GenderConst,default=GenderConst.Male)
     adress = models.CharField(max_length=100)
-    contact = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
+    contact = models.CharField(max_length=100) 
+    created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    class Meta:
+        db_table='assistant_doctor'
+        ordering = ['created']
+        verbose_name = 'assistant_doctor'
+        verbose_name_plural = 'assistants_doctor'
+
+
     
 
 class Rendez_vous(models.Model):
-    id_doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
-    id_patient = models.ForeignKey(Patient,on_delete=models.PROTECT)
+    doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
+    patient = models.ForeignKey(Patient,on_delete=models.PROTECT)
     date = models.DateField()
     description = models.TextField()
     heure_arrive = models.TimeField()
     heure_fin = models.TimeField()
     status = models.CharField(max_length=10,choices=Rendez_vousConst,default=Rendez_vousConst.Pending)
-
+    created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return f"RDV - {self.doctor} {self.patient} {self.date} {self.status}"
